5e8fd00a92240adda37ca5b775089ebc7844fe1a
meq1986 <meq1986@163.com>
2017-11-28 21:50:16 +0800

    test some string

--- CSharp-learning/Program.cs
+++ CSharp-learning/Program.cs
@@ -10,6 +10,17 @@ namespace CSharp_learning
         static void Main(string[] args)
         {
             Console.WriteLine("Hello,world.");
+            String str = "sex=1,year=1985,month=4,day=1,hour=0,minute=0,sex2=0,year2=1985,month2=4,day2=1,hour2=0,minute2=0";
+            String[] arr = str.Split(',');
+
+            foreach(String t in arr)
+            {
+                Console.WriteLine(t);
+                String[] keyvalue = t.Split('=');
+                Console.WriteLine(keyvalue[1]);
+            }
+
+            Console.Read();
         }
     }
 }
